cf91949f799d33030675470a19031c08269f841f
Paulo Oliveira <paul8liveira@gmail.com>
2025-01-30 14:52:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,39 +1,40 @@
 // create a web server
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
+// create a route to handle comments
+// create a route to handle comments/new
+// create a route to handle comments/:id
+// create a route to handle comments/:id/edit
+// create a route to handle comments/:id/delete
 
-// create a web server
-const express = require('express')
-const app = express()
+// 1. create a web server
+const express = require('express');
+const app = express();
 
-// create a route for comments
+// 2. create a route to handle comments
 app.get('/comments', (req, res) => {
-  res.send('comments')
-})
+  res.send('This is the comments page');
+});
 
-// create a route for comments/new
+// 3. create a route to handle comments/new
 app.get('/comments/new', (req, res) => {
-  res.send('new comments')
-})
+  res.send('This is the new comments page');
+});
 
-// create a route for comments/:id
+// 4. create a route to handle comments/:id
 app.get('/comments/:id', (req, res) => {
-  res.send('comments id')
-})
+  res.send('This is the comments page for id: ' + req.params.id);
+});
 
-// create a route for comments/:id/edit
+// 5. create a route to handle comments/:id/edit
 app.get('/comments/:id/edit', (req, res) => {
-  res.send('edit comments')
-})
+  res.send('This is the edit comments page for id: ' + req.params.id);
+});
 
-// create a route for comments/:id/delete
+// 6. create a route to handle comments/:id/delete
 app.get('/comments/:id/delete', (req, res) => {
-  res.send('delete comments')
-})
+  res.send('This is the delete comments page for id: ' + req.params.id);
+});
 
+// 7. listen on port 3000
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000');
-})
+  console.log('Server is running on port 3000');
+});
