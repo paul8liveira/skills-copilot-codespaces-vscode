8b0f8dcd0a749931b718fd92bb9007ba63d6c7eb
Paulo Oliveira <paul8liveira@gmail.com>
2025-01-30 14:26:30 +0000

    Second copilot commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMenber()
+function skillsMember(name, skills) {
+    this.name = name;
+    this.skills = skills;
+
+    this.addSkill = function(skill) {
+        this.skills.push(skill);
+    };
+
+    this.getSkills = function() {
+        return this.skills;
+    };
+}
